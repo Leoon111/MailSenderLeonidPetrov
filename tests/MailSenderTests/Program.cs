eef7d2329faae3bc05790a129b61e485b8aa40e8
Leonid Petrov <leoon111@gmail.com>
2020-09-25 04:09:59 +0300

    Добавьте файлы проекта.

---
+++ tests/MailSenderTests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace MailSenderTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
