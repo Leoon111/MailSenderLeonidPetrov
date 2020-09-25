d21524c2fecb2e0b6079ad62af7a60283ce534d3
Leonid Petrov <leoon111@gmail.com>
2020-09-25 04:40:41 +0300

    Тестовое консольное приложение

--- tests/MailSenderTests/Program.cs
+++ tests/MailSenderTests/Program.cs
@@ -8,7 +8,22 @@ namespace MailSenderTests
     {
         static void Main(string[] args)
         {
-            
+            var to = new MailAddress("Leoon111@gmail.com");
+            var from = new MailAddress("Leoon123@yandex.ru");
+
+            var message = new MailMessage(from, to);
+
+            message.Subject = "Заголовок письма + " + DateTime.Now;
+            message.Body = "Тело письма " + DateTime.Now;
+
+            var client = new SmtpClient("smtp.yandex.ru", 25);
+
+            client.Credentials = new NetworkCredential
+            {
+                UserName = "user@name.com",
+                Password = "password"
+            };
+            client.Send(message);
         }
     }
 }
