a6aef831f09e844f250ce074c162ab98b0e7ecbb
Leonid Petrov <leoon111@gmail.com>
2020-10-08 22:28:10 +0300

    Тестовая модель представления

--- WpfMailSenderTestLeonidPetrov/WpfMailSenderTestLeonidPetrov/ViewModels/MainWindowViewModel.cs
+++ WpfMailSenderTestLeonidPetrov/WpfMailSenderTestLeonidPetrov/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Timers;
 using WpfMailSenderTestLeonidPetrov.ViewModels.Base;
 
 namespace WpfMailSenderTestLeonidPetrov.ViewModels
@@ -12,12 +13,29 @@ namespace WpfMailSenderTestLeonidPetrov.ViewModels
         public string Title
         {
             get => _Title;
-            set
-            {
-                if(_Title == value) return;
-                _Title = value;
-                OnPropertyChanged("Title");
-            }
+            set => Set(ref _Title, value);
+            //{
+            //    if(_Title == value) return;
+            //    _Title = value;
+            //    //OnPropertyChanged("Title");
+            //    //OnPropertyChanged(nameof(Title));
+            //    OnPropertyChanged();
+            //}
+        }
+        public  DateTime CurrentTime => DateTime.Now;
+        private readonly Timer _timer;
+
+        public MainWindowViewModel()
+        {
+            _timer = new Timer(100);
+            _timer.Elapsed += OnTimerElapsed;
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs E)
+        {
+            OnPropertyChanged(nameof(CurrentTime));
         }
     }
 }
