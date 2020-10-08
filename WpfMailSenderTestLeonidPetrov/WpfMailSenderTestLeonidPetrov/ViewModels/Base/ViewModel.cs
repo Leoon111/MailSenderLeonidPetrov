c8a3ea656ad477416324775fc43b0231ec084c3d
Leonid Petrov <leoon111@gmail.com>
2020-10-08 21:56:14 +0300

    Реализация модели представления

---
+++ WpfMailSenderTestLeonidPetrov/WpfMailSenderTestLeonidPetrov/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using WpfMailSenderTestLeonidPetrov.Annotations;
+
+namespace WpfMailSenderTestLeonidPetrov.ViewModels.Base
+{
+    abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WpfMailSenderTestLeonidPetrov/WpfMailSenderTestLeonidPetrov/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WpfMailSenderTestLeonidPetrov.ViewModels.Base;
+
+namespace WpfMailSenderTestLeonidPetrov.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Тестовое окно";
+
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                if(_Title == value) return;
+                _Title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+    }
+}
