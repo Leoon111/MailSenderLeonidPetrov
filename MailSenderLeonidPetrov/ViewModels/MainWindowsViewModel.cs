1a5ec8b951fea6e0da2c0dcb84c85d03c54db6f6
Leonid Petrov <leoon111@gmail.com>
2020-10-09 18:38:53 +0300

    Привязка адреса, получателя, отправителя

--- MailSenderLeonidPetrov/ViewModels/MainWindowsViewModel.cs
+++ MailSenderLeonidPetrov/ViewModels/MainWindowsViewModel.cs
@@ -1,4 +1,7 @@
-﻿using MailSenderLeonidPetrov.ViewModels.Base;
+﻿using System.Collections.ObjectModel;
+using MailSenderLeonidPetrov.Data;
+using MailSenderLeonidPetrov.Models;
+using MailSenderLeonidPetrov.ViewModels.Base;
 
 namespace MailSenderLeonidPetrov.ViewModels
 {
@@ -10,7 +13,41 @@ namespace MailSenderLeonidPetrov.ViewModels
         {
             get => _Title;
             set => Set(ref _Title, value);
-           
+        }
+
+        private ObservableCollection<Server> _Servers;
+        private ObservableCollection<Sender> _Senders;
+        private ObservableCollection<Recipient> _Recipients;
+        private ObservableCollection<Message> _Messages;
+
+        public ObservableCollection<Server> Servers
+        {
+            get => _Servers;
+            set => Set(ref _Servers, value);
+        }
+        public ObservableCollection<Sender> Senders
+        {
+            get => _Senders;
+            set => Set(ref _Senders, value);
+        }
+        public ObservableCollection<Recipient> Recipients
+        {
+            get => _Recipients;
+            set => Set(ref _Recipients, value);
+        }
+        public ObservableCollection<Message> Messages
+        {
+            get => _Messages;
+            set => Set(ref _Messages, value);
+        }
+
+        public MainWindowsViewModel()
+        {
+            Servers = new ObservableCollection<Server>(TestData.Servers);
+            Senders = new ObservableCollection<Sender>(TestData.Senders);
+            Recipients = new ObservableCollection<Recipient>(TestData.Recipients);
+            Messages = new ObservableCollection<Message>(TestData.Messages);
+
         }
     }
 }
