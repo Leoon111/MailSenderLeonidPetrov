1491e13e8cf7a2dd67ac04ffc707b8106a9a91e4
Leonid Petrov <leoon111@gmail.com>
2020-10-10 18:52:09 +0300

    Команды отправки почты

--- MailSenderLeonidPetrov/ViewModels/MainWindowsViewModel.cs
+++ MailSenderLeonidPetrov/ViewModels/MainWindowsViewModel.cs
@@ -138,6 +138,35 @@ namespace MailSenderLeonidPetrov.ViewModels
         }
 
         #endregion
+        #region SentMailCommand
+
+        private ICommand _SentMailCommand;
+
+        public ICommand SentMailCommand => _SentMailCommand
+            ??= new LambdaCommand(OnSentMailCommandExecuted, CanSentMailCommandExecute);
+
+        private bool CanSentMailCommandExecute(object p)
+        {
+            if (SelectedServer is null) return false;
+            if (SelectedSender is null) return false;
+            if (SelectedRecipient is null) return false;
+            if (SelectedMessage is null) return false;
+            return true;
+        }
+
+        private void OnSentMailCommandExecuted(object p)
+        {
+            var server = SelectedServer;
+            var sender = SelectedSender;
+            var recipient = SelectedRecipient;
+            var message = SelectedMessage;
+
+            var mail_sender = _mailService.GetSender(server.Address, server.Port, server.UseSSL, server.Login,
+                server.Password);
+            mail_sender.Send(sender.Address, recipient.Address, message.Subject, message.Body);
+        }
+
+        #endregion
 
         #endregion
 
