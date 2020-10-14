3e436507f933962c7da81bdf0b92083112b4509a
Leonid Petrov <leoon111@gmail.com>
2020-10-14 20:50:50 +0300

    Слияние 2

--- MailSenderLeonidPetrov/App.xaml.cs
+++ MailSenderLeonidPetrov/App.xaml.cs
@@ -1,31 +1,17 @@
-﻿using Microsoft.Extensions.Hosting;
-using System;
-using MailSender.lib.Interface;
-using MailSender.lib.Service;
-using MailSenderLeonidPetrov.ViewModels;
-using Microsoft.Extensions.DependencyInjection;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace MailSenderLeonidPetrov
 {
-    public partial class App
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
     {
-        private static IHost _Hosting;
-
-        public static IHost Hosting => _Hosting
-            ??= Host.CreateDefaultBuilder(Environment.GetCommandLineArgs())
-                .ConfigureServices(ConfigureServices)
-                .Build();
-
-        public static IServiceProvider Services => Hosting.Services;
-
-        private static void ConfigureServices(HostBuilderContext host, IServiceCollection service)
-        {
-            service.AddSingleton<MainWindowsViewModel>();
-#if DEBUG
-            service.AddTransient<IMailService, DebugMailService>();
-#else
-            service.AddTransient<IMailService, SmtpMailService>();
-#endif
-        }
     }
 }
