5be343704a0c4326fa33b8cc433f2c9a52aa8a31
Leonid Petrov <leoon111@gmail.com>
2020-10-14 21:57:25 +0300

    Представление списка получателей почты

--- MailSenderLeonidPetrov/MainWindow.xaml.cs
+++ MailSenderLeonidPetrov/MainWindow.xaml.cs
@@ -15,36 +15,6 @@ namespace MailSenderLeonidPetrov
         public MainWindow()
         {
             InitializeComponent();
-            //ServersList.ItemsSource = TestData.Senders;
-        }
-
-        private void OnSendButtonClick(object sender, RoutedEventArgs e)
-        {
-            var mySender = SendersList.SelectedItem as Sender;
-            if(mySender is null) return;
-
-            if(!(RecipientsList.SelectedItem is Recipient recipient)) return;
-            if (!(SendersList.SelectedItem is Server server)) return;
-            if (!(MessegesList.SelectedItems is Message message)) return;
-
-            var send_service = new MailSenderService()
-            {
-                ServerAddress = server.Address,
-                ServerPort = server.Port,
-                UseSSL = server.UseSSL,
-                Login = server.Login,
-                Passvord = server.Password
-            };
-
-            try
-            {
-                send_service.SendMessage(mySender.Address, recipient.Address, message.Subject, message.Body);
-            }
-            catch (SmtpException error)
-            {
-                MessageBox.Show($"Ошибка при отправке почты {error.Message}", "Ошибка", MessageBoxButton.OK,
-                    MessageBoxImage.Error);
-            }
         }
     }
 }
