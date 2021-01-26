f972ebaa89079a6eefe287661397c511f0ebc265
Леонид <leooncom@hotmail.com>
2021-01-26 21:37:25 +0300

    Примеры использования async-await с интерфейсом пользователя

--- WpfMailSenderTestLeonidPetrov/WpfMailSenderTestLeonidPetrov/MainWindow.xaml.cs
+++ WpfMailSenderTestLeonidPetrov/WpfMailSenderTestLeonidPetrov/MainWindow.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Win32;
 
 namespace WpfMailSenderTestLeonidPetrov
 {
@@ -13,43 +19,151 @@ namespace WpfMailSenderTestLeonidPetrov
             InitializeComponent();
         }
 
-        private void ComputeResultButton_Click(object sender, RoutedEventArgs e)
-        {
-            // здесь в один поток
-            //var result = GetResultHard();
-            //ResultText.Text = result;
+        //private void ComputeResultButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    // здесь в один поток
+        //    //var result = GetResultHard();
+        //    //ResultText.Text = result;
 
-            new Thread(() =>
+        //    new Thread(() =>
+        //    {
+        //        // так нельзя, потому что Интерфейс - это критическая секция и будет ругаться, что не из потока интерфейса.
+        //        //ResultText.Text = GetResultHard();
+
+        //        // либо
+        //        //Dispatcher
+        //        //ResultText.Dispatcher
+        //        //App.Current.Dispatcher
+
+        //        var result = GetResultHard();
+        //        UpdateResultValue(result);
+        //    }){ IsBackground = true}.Start();
+        //}
+
+        //public void UpdateResultValue(string Result)
+        //{
+        //    if (Dispatcher.CheckAccess())
+        //        ResultText.Text = Result;
+        //    else
+        //        Dispatcher.Invoke(() => UpdateResultValue(Result));
+        //}
+
+        //private string GetResultHard()
+        //{
+        //    for (int i = 0; i < 500; i++)
+        //    {
+        //        Thread.Sleep(10);
+        //    }
+
+        //    return "Hello!";
+        //}
+        private async void OnOpenFileClick(object sender, RoutedEventArgs e)
+        {
+            // Мы находились в TheradId == 1
+            await Task.Yield(); // Даем время на обработку сообщений пользовательского интерфейса
+            // Мы снова в ThreadID == 1
+
+            string filePath =
+                @"E:\MyApps\C#\C# Уровень 3\MailSenderLeonidPetrov\WpfMailSenderTestLeonidPetrov\WpfMailSenderTestLeonidPetrov";
+
+            var dialog = new OpenFileDialog
             {
-                // так нельзя, потому что Интерфейс - это критическая секция и будет ругаться, что не из потока интерфейса.
-                //ResultText.Text = GetResultHard();
+                Title = "Выбор файла для чтения",
+                Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*",
+                RestoreDirectory = true,
+                InitialDirectory = Directory.Exists(filePath)
+                    ? filePath
+                    : string.Empty
+            };
 
-                // либо
-                //Dispatcher
-                //ResultText.Dispatcher
-                //App.Current.Dispatcher
 
-                var result = GetResultHard();
-                UpdateResultValue(result);
-            }){ IsBackground = true}.Start();
-        }
+            if (dialog.ShowDialog() != true) return;
 
-        public void UpdateResultValue(string Result)
-        {
-            if (Dispatcher.CheckAccess())
-                ResultText.Text = Result;
-            else
-                Dispatcher.Invoke(() => UpdateResultValue(Result));
-        }
+            //var dict = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            //using (var reader = new StreamReader(dialog.FileName))
+            //{
+            //    while (!reader.EndOfStream)
+            //    {
+            //        var line = await reader.ReadLineAsync().ConfigureAwait(false);
+            //        var words = line.Split(' ');
+            //        //Thread.Sleep(100);
+            //        //await Task.Delay(1);
+            //        foreach (var word in words)
+            //            if (dict.ContainsKey(word))
+            //                dict[word]++;
+            //            else
+            //                dict.Add(word, 1);
+            //        //ProgressInfo.Value = reader.BaseStream.Position / (double)reader.BaseStream.Length;
+            //        ProgressInfo.Dispatcher.Invoke(() =>
+            //            ProgressInfo.Value = reader.BaseStream.Position / (double) reader.BaseStream.Length);
+            //    }
+            //}
+            //var count = dict.Count;
+            //Result.Text = $"Число слов {count}";
+            //Result.Dispatcher.Invoke(() => Result.Text = $"Число слов {count}");
 
-        private string GetResultHard()
-        {
-            for (int i = 0; i < 500; i++)
+            StartAction.IsEnabled = false;
+            CancelAction.IsEnabled = true;
+
+            _ReadingFileCancellation = new CancellationTokenSource();
+
+            var cancel = _ReadingFileCancellation.Token;
+            IProgress<double> progress = new Progress<double>(p => ProgressInfo.Value = p);
+
+            try
             {
-                Thread.Sleep(10);
+                var count = await GetWordsCountAsync(dialog.FileName, progress, cancel);
+                Result.Text = $"Число слов {count}";
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.WriteLine("Операция чтения файла была отменена");
+                Result.Text = $"---";
+                progress.Report(0);
             }
 
-            return "Hello!";
+            CancelAction.IsEnabled = false;
+            StartAction.IsEnabled = true;
+        }
+
+        private static async Task<int> GetWordsCountAsync(string FileName, IProgress<double> progress = null, CancellationToken cansel = default)
+        {
+            // Мы находимся в ThreadId == 7 (к примеру)
+            await Task.Yield();
+            // Теперь мы в ThreadId == 12 (например) (а возможно и обратно в 7)
+
+            var dict = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            cansel.ThrowIfCancellationRequested();
+            using (var reader = new StreamReader(FileName))
+            {
+                while (!reader.EndOfStream)
+                {
+                    cansel.ThrowIfCancellationRequested();
+                    var line = await reader.ReadLineAsync().ConfigureAwait(false);
+                    // ConfigureAwait(false); - требование "вернутся" в произвольны поток из пула потоков.
+                    var words = line.Split(' ');
+
+                    //Thread.Sleep(100);
+                    await Task.Delay(1);
+
+                    foreach (var word in words)
+                        if (dict.ContainsKey(word))
+                            dict[word]++;
+                        else
+                            dict.Add(word, 1);
+
+                    progress?.Report(reader.BaseStream.Position / (double)reader.BaseStream.Length);
+                }
+            }
+
+            return dict.Count;
+        }
+
+        private CancellationTokenSource _ReadingFileCancellation;
+
+        private void OnCancelReadingClick(object sender, RoutedEventArgs e)
+        {
+            _ReadingFileCancellation?.Cancel();
         }
     }
 }
